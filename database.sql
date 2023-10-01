28574949a5ae7cd68b0a26d386283a77cb25c5fd
yuliaannt <yuliaeka.ananta@gmail.com>
2023-10-01 13:54:45 +0700

    revisi

--- database.sql
+++ database.sql
@@ -4,17 +4,17 @@ CREATE TABLE IF NOT EXISTS sayur (
   id_sayur INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   nama_sayur VARCHAR(30) NOT NULL,
   gambar_sayur BLOB,
-  tanggal_input DATETIME,
-) engine = InnoDB;
+  tanggal_input DATETIME
+) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS pasar(
   id_pasar INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   nama_pasar VARCHAR(30) NOT NULL,
-) engine = InnoDB;
+) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS deteksi (
   id_deteksi INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  id_sayur INT AUTO_INCREMENT NOT NULL,
-  id_pasar INT AUTO_INCREMENT NOT NULL,
+  id_sayur INT NOT NULL,
+  id_pasar INT NOT NULL,
   hasil_deteksi VARCHAR(30),
   FOREIGN KEY (id_sayur) REFERENCES sayur(id_sayur),
   FOREIGN KEY (id_pasar) REFERENCES pasar(id_pasar)
-) engine = InnoDB;
+) ENGINE = InnoDB;
