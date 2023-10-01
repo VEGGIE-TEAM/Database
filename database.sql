fb5d698648f49e66328eba160607d7d311c4f4c5
yuliaannt <yuliaeka.ananta@gmail.com>
2023-10-01 13:14:58 +0700

    id auto increment

--- database.sql
+++ database.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE IF NOT EXISTS veggie_db;
 USE veggie_db;
 CREATE TABLE IF NOT EXISTS sayur (
-  id_sayur VARCHAR(10) NOT NULL PRIMARY KEY,
+  id_sayur INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   nama_sayur VARCHAR(30) NOT NULL,
   gambar_sayur BLOB,
   tanggal_input DATETIME,
 ) engine = InnoDB;
 CREATE TABLE IF NOT EXISTS pasar(
-  id_pasar VARCHAR(30) NOT NULL PRIMARY KEY,
+  id_pasar INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   nama_pasar VARCHAR(30) NOT NULL,
 ) engine = InnoDB;
 CREATE TABLE IF NOT EXISTS deteksi (
-  id_deteksi VARCHAR(30) NOT NULL PRIMARY KEY,
-  id_sayur VARCHAR(10) NOT NULL,
-  id_pasar VARCHAR(30) NOT NULL,
+  id_deteksi INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id_sayur INT AUTO_INCREMENT NOT NULL,
+  id_pasar INT AUTO_INCREMENT NOT NULL,
   hasil_deteksi VARCHAR(30),
   FOREIGN KEY (id_sayur) REFERENCES sayur(id_sayur),
   FOREIGN KEY (id_pasar) REFERENCES pasar(id_pasar)
